7d26cb1516bdb3a2b3c9a10e0e0f220206fe5d17
Björn Zeutzheim <bjoern@zeutzheim-boppard.de>
2015-10-22 15:54:10 +0900

    Bumped version to 1.2

--- src/main/java/com/forgeessentials/customserveritems/CustomServerItems.java
+++ src/main/java/com/forgeessentials/customserveritems/CustomServerItems.java
@@ -31,7 +31,7 @@ public class CustomServerItems implements IMessageHandler<PacketRequestTexture, 
 
     public static final String MODID = "customserveritems";
 
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
 
     public static final String TAG_TEXTURE = "csi_texture";
 
